65fc7e4faf7a6a7abe9c23a16f99c627770d8c9c
Gustavo GM (Goz) <gozunam@gmail.com>
2025-06-26 16:50:06 -0600

    Add first version o file script 8 and new info in documentation

---
+++ files_scripts/file_script_8.py
@@ -0,0 +1,35 @@
+#######################################################################
+#       __
+#      (00)         Code With Goz - https://codewithgoz.com
+#      \__/         Curso: Automatiza Tareas con Python
+#      /^/          Autor: Goz
+#     ( (            
+#     \_\_____      Automatización de Archivos y Directorios   
+#     (_______)     file_script_8.py
+#   (___________)   Listar archivos dentro del árbol de directorios
+#  (_____________)  utilizando la biblioteca pathlib
+#######################################################################
+
+from pathlib import Path
+
+# Obtenemos el directorio actual
+my_current_path = Path('.')
+print(my_current_path)
+
+# Obtenemos los archivos dentro del árbol de directorios
+my_python_scripts = list(my_current_path.glob('**/*.py'))
+
+print(my_python_scripts)
+
+for my_script in my_python_scripts:
+    print(my_script)
+
+
+
+
+
+
+
+
+
+    
